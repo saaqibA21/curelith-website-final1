8d5bcdb57f424c604be12f8dd46fbd642988a475
saaqibA21 <149220025+saaqibA21@users.noreply.github.com>
2025-08-04 22:33:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ async function sendMessage() {
     sendFollowupDiagnosis(text);
   } else {
     try {
-      const res = await fetch(`${BASE_URL}/ask`, {
+      const res = await fetch(`${BASE_URL}/ask/`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ question: text })
